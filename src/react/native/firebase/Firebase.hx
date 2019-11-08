13258ba41331941090563530ede75709b9102b37
Jarrio <Jarrio@users.noreply.github.com>
2019-11-08 14:22:27 +0000

    Added support for node.js firebase

--- src/react/native/firebase/Firebase.hx
+++ src/react/native/firebase/Firebase.hx
@@ -7,12 +7,16 @@ import react.native.firebase.firestore.Firestore;
 import react.native.firebase.database.Database;
 import react.native.firebase.types.TFirebaseAppOptions;
 
+#if react-native
 @:jsRequire('@react-native-firebase/app', 'default')
+#else
+@:jsRequire('firebase')
+#end
 extern class Firebase {
 	public static var apps:Array<FirebaseApp>;
 	public static function app(?name:String):FirebaseApp;	
 	public static function setLogLevel(level:Dynamic):Void;
-	public static function initializeApp(?config:TFirebaseAppOptions, name:String = "[DEFAULT]"):Promise<FirebaseApp>;
+	public static function initializeApp(?config:TFirebaseAppOptions, name:String = "[DEFAULT]"):#if react-native Promise<FirebaseApp> #else FirebaseApp #end;
 	public static function auth(?app:FirebaseApp):Auth;
 	public static function messaging(?app:FirebaseApp):Messaging;	
 	public static function database(?app:FirebaseApp):Database;	
