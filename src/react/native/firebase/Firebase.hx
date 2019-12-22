ca9637dac04fa8de8db7c432633d8e11cff4f0ef
Jarrio <Jarrio@users.noreply.github.com>
2019-12-22 20:37:59 +0000

    fixed define

--- src/react/native/firebase/Firebase.hx
+++ src/react/native/firebase/Firebase.hx
@@ -7,24 +7,23 @@ import react.native.firebase.firestore.Firestore;
 import react.native.firebase.database.Database;
 import react.native.firebase.types.TFirebaseAppOptions;
 
-#if react-native
+#if reactnative
 @:jsRequire('@react-native-firebase/app', 'default')
 #else
 @:jsRequire('firebase')
 #end
 extern class Firebase {
 	public static var apps:Array<FirebaseApp>;
-	public static function app(?name:String):FirebaseApp;	
+	public static function app(?name:String):FirebaseApp;
 	public static function setLogLevel(level:Dynamic):Void;
-	public static function initializeApp(?config:TFirebaseAppOptions, name:String = "[DEFAULT]"):#if react-native Promise<FirebaseApp> #else FirebaseApp #end;
+	public static function initializeApp(?config:TFirebaseAppOptions, name:String = "[DEFAULT]"):#if react
+		- native Promise<FirebaseApp> #else FirebaseApp #end;
 	public static function auth(?app:FirebaseApp):Auth;
-	public static function messaging(?app:FirebaseApp):Messaging;	
-	public static function database(?app:FirebaseApp):Database;	
-	public static function firestore(?app:FirebaseApp):Firestore;	
-
-
+	public static function messaging(?app:FirebaseApp):Messaging;
+	public static function database(?app:FirebaseApp):Database;
+	public static function firestore(?app:FirebaseApp):Firestore;
 	// public static function database(?app:firebase.app.App):firebase.database.Database;
 	// public static function auth(?app:firebase.app.App):firebase.auth.Auth;
 	// public static function storage(?app:firebase.app.App):firebase.storage.Storage;
-	// public static function messaging(?app:firebase.app.App):firebase.messaging.Messaging;	
+	// public static function messaging(?app:firebase.app.App):firebase.messaging.Messaging;
 }
